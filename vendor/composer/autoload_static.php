401c290323e79bf0bebc18a02df2bad89ab82dca
bossaiguo <1058349313@qq.com>
2019-05-07 15:38:08 +0800

    optimization install route

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5774ccb2b4c73f5f03c1b8641e00f77a
 {
+    public static $prefixLengthsPsr4 = array (
+        'N' => 
+        array (
+            'NoahBuscher\\Macaw\\' => 18,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'NoahBuscher\\Macaw\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/noahbuscher/macaw',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit5774ccb2b4c73f5f03c1b8641e00f77a::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit5774ccb2b4c73f5f03c1b8641e00f77a::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
